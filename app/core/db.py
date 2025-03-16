cd263a250930289ffc60c67d2f337e78a1dbae44
Vladislav Vasilev <vladen21@yandex.ru>
2025-03-16 11:16:36 +0300

    Спринт 22/27 → Тема 2/4: База данных в FastAPI → Урок 2/2: Подключение базы данных и SQLAlchemy к FastAPI

--- app/core/db.py
+++ app/core/db.py
@@ -1,3 +1,10 @@
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine, async_sessionmaker
 from sqlalchemy.orm import declarative_base
 
+from app.core.config import settings
+
 Base = declarative_base()
+engine = create_async_engine(settings.database_url)
+
+AsyncSessionLocal = async_sessionmaker(engine, class_=AsyncSession)
+
