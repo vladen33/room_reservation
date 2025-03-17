2738dfc32df25d8fd3e74d0d6ee63dec13383b40
Vladislav Vasilev <vladen21@yandex.ru>
2025-03-17 22:01:02 +0300

    Спринт 22/27 → Тема 3/4: Миграции: библиотека Alembic → Урок 2/3: Автоматическое создание миграций

--- alembic/env.py
+++ alembic/env.py
@@ -9,6 +9,7 @@ from sqlalchemy.ext.asyncio import async_engine_from_config
 
 from alembic import context
 
+from app.core.base import Base
 
 load_dotenv('.env')
 # this is the Alembic Config object, which provides
@@ -25,7 +26,7 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+target_metadata = Base.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
--- app/core/db.py
+++ app/core/db.py
@@ -1,10 +1,19 @@
+from sqlalchemy import Column, Integer
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine, async_sessionmaker
-from sqlalchemy.orm import declarative_base
+from sqlalchemy.orm import declarative_base, declared_attr
 
 from app.core.config import settings
 
-Base = declarative_base()
+
+class PreBase:
+    @declared_attr
+    def __tablename__(cls):
+        return cls.__name__.lower()
+
+    id = Column(Integer, primary_key=True)
+
+
+Base = declarative_base(cls=PreBase)
 engine = create_async_engine(settings.database_url)
 
 AsyncSessionLocal = async_sessionmaker(engine, class_=AsyncSession)
-
