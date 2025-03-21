1f9e6607d349233218dd318643eeac99ce50ea58
Vladislav Vasilev <vladen21@yandex.ru>
2025-03-21 22:05:12 +0300

    Спринт 22/27 → Тема 4/4: CRUD в FastAPI → Урок 3/7

--- app/schemas/meeting_room.py
+++ app/schemas/meeting_room.py
@@ -3,6 +3,17 @@ from typing import Optional
 from pydantic import BaseModel, Field
 
 
-class MeetingRoomCreate(BaseModel):
-    name: str = Field(..., min_length=1, max_length=100)
+class MeetingRoomBase(BaseModel):
+    name: Optional[str] = Field(None, min_length=1, max_length=100)
     description: Optional[str]
+
+
+class MeetingRoomCreate(MeetingRoomBase):
+    name: str = Field(..., min_length=1, max_length=100)
+
+
+class MeetingRoomDB(MeetingRoomCreate):
+    id: int
+
+    class Config:
+        orm_mode = True
