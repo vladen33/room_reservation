c80beca97871bdfafbf56a8c7aa0cdfc99cf5a10
Vladislav Vasilev <vladen21@yandex.ru>
2025-03-22 14:19:04 +0300

    Спринт 22/27 → Тема 4/4: CRUD в FastAPI → Урок 4/7: Внедрение зависимостей (Dependency Injection)

--- app/api/meeting_room.py
+++ app/api/meeting_room.py
@@ -1,5 +1,7 @@
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy.ext.asyncio import AsyncSession
 
+from app.core.db import get_async_session
 from app.crud.meeting_room import create_meeting_room, get_room_id_by_name
 from app.schemas.meeting_room import MeetingRoomCreate, MeetingRoomDB
 
@@ -11,12 +13,15 @@ router = APIRouter()
     response_model=MeetingRoomDB,
     response_model_exclude_none=True
 )
-async def create_new_meeting_room(meeting_room: MeetingRoomCreate):
-    room_id = await get_room_id_by_name(meeting_room.name)
+async def create_new_meeting_room(
+        meeting_room: MeetingRoomCreate,
+        session: AsyncSession = Depends(get_async_session)
+):
+    room_id = await get_room_id_by_name(meeting_room.name, session)
     if room_id is not None:
         raise HTTPException(
             status_code=422,
             detail='Переговорка с таким именем уже есть в базе.'
         )
-    new_room = await create_meeting_room(meeting_room)
+    new_room = await create_meeting_room(meeting_room, session)
     return new_room
--- app/crud/meeting_room.py
+++ app/crud/meeting_room.py
@@ -1,26 +1,31 @@
 from typing import Optional
 
 from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.core.db import AsyncSessionLocal
 from app.models.meeting_room import MeetingRoom
 from app.schemas.meeting_room import MeetingRoomCreate
 
 
-async def create_meeting_room(new_room: MeetingRoomCreate) -> MeetingRoom:
+async def create_meeting_room(
+        new_room: MeetingRoomCreate,
+        session: AsyncSession
+) -> MeetingRoom:
     new_room_data = new_room.dict()
     db_room = MeetingRoom(**new_room_data)
-    async with AsyncSessionLocal() as session:
-        session.add(db_room)
-        await session.commit()
-        await session.refresh(db_room)
+    session.add(db_room)
+    await session.commit()
+    await session.refresh(db_room)
     return db_room
 
 
-async def get_room_id_by_name(room_name: str) -> Optional[int]:
-    async with AsyncSessionLocal() as session:
-        db_room_id = await session.execute(
-            select(MeetingRoom.id).where(MeetingRoom.name == room_name)
-        )
-        db_room_id = db_room_id.scalars().first()
+async def get_room_id_by_name(
+        room_name: str,
+        session: AsyncSession
+) -> Optional[int]:
+    db_room_id = await session.execute(
+        select(MeetingRoom.id).where(MeetingRoom.name == room_name)
+    )
+    db_room_id = db_room_id.scalars().first()
     return db_room_id
