c0ca4598b68d0b82c7a65b80fe6d253cabad47a1
Vladislav Vasilev <vladen21@yandex.ru>
2025-03-24 21:29:50 +0300

    Спринт 23/27 → Тема 1/3: Модель бронирования переговорок → Урок 1/8: Модель Reservation. Связь моделей

--- app/core/base.py
+++ app/core/base.py
@@ -1,2 +1,3 @@
 from app.core.db import Base
 from app.models.meeting_room import MeetingRoom
+from app.models.reservation import Reservation
--- app/models/meeting_room.py
+++ app/models/meeting_room.py
@@ -1,15 +1,14 @@
 # app/models/meeting_room.py
 
 # Импортируем из Алхимии нужные классы.
-from sqlalchemy import Column, String, Text, Integer
+from sqlalchemy import Column, String, Text
+from sqlalchemy.orm import relationship
 
 # Импортируем базовый класс для моделей.
 from app.core.db import Base
 
 
 class MeetingRoom(Base):
-    # Имя переговорки должно быть не больше 100 символов,
-    # уникальным и непустым.
     name = Column(String(100), unique=True, nullable=False)
     description = Column(Text)
-    # size = Column(Integer)
+    reservations = relationship('Reservation', cascade='delete')
