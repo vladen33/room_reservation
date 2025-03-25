fa1b69675e15fd5337d486bbf37cf669345dc6ad
Vladislav Vasilev <vladen21@yandex.ru>
2025-03-25 22:35:48 +0300

    Спринт 23/27 → Тема 1/3: Модель бронирования переговорок → Урок 4/8: Объединение роутеров и создание файла с валидаторами

--- app/api/endpoints/meeting_room.py
+++ app/api/endpoints/meeting_room.py
@@ -1,13 +1,14 @@
-from fastapi import APIRouter, Depends, HTTPException
+from fastapi import APIRouter, Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 
+from app.api.validators import check_meeting_room_exists, check_name_duplicate
 from app.core.db import get_async_session
 from app.crud.meeting_room import meeting_room_crud
-from app.models.meeting_room import MeetingRoom
 from app.schemas.meeting_room import (
     MeetingRoomCreate, MeetingRoomDB, MeetingRoomUpdate)
 
-router = APIRouter(prefix='/meeting_rooms', tags=['Meeting Rooms'])
+
+router = APIRouter()
 
 
 @router.post(
@@ -82,30 +83,3 @@ async def remove_meeting_room(
     return meeting_room
 
 
-# Корутина, проверяющая уникальность полученного имени переговорки.
-async def check_name_duplicate(
-        room_name: str,
-        session: AsyncSession,
-) -> None:
-    room_id = await meeting_room_crud.get_room_id_by_name(room_name, session)
-    if room_id is not None:
-        raise HTTPException(
-            status_code=422,
-            detail='Переговорка с таким именем уже существует!',
-        )
-
-
-# Оформляем повторяющийся код в виде отдельной корутины.
-async def check_meeting_room_exists(
-        meeting_room_id: int,
-        session: AsyncSession,
-) -> MeetingRoom:
-    meeting_room = await meeting_room_crud.get(
-        meeting_room_id, session
-    )
-    if meeting_room is None:
-        raise HTTPException(
-            status_code=404,
-            detail='Переговорка не найдена!'
-        )
-    return meeting_room
