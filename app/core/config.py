315c67092d32bc6fe6fdb6b04546593d78a42aca
Vladislav Vasilev <vladen21@yandex.ru>
2025-03-29 20:31:27 +0300

    Спринт 23/27 → Тема 2/3: FastAPI Users → Урок 3/8: Настройки библиотеки FastAPI Users

--- app/core/config.py
+++ app/core/config.py
@@ -5,6 +5,7 @@ class Settings(BaseSettings):
     app_title: str = 'Бронирование переговорок'
     description: str = 'Описание проекта по бронированию переговорок'
     database_url: str
+    secret: str = 'SECRET'
 
     class Config:
         extra = 'ignore'
